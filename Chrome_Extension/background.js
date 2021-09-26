58c9c8f763e0b6ed5e7c21eaae3598c9d7f76131
omaramr3410 <omaramr3410@gmail.com>
2021-09-26 10:58:03 -0400

    updating costar again, with parsons

--- Chrome_Extension/background.js
+++ Chrome_Extension/background.js
@@ -77,9 +77,10 @@ let costar = async () => {
 
 				new_obj.name = name;
 				new_obj.link = link;
+				job_info = job_info.replace(/<\/?[^>]+(>|$)/g, "");
 				new_obj.job_info = job_info;
 				// const start = "RESPONSIBILITIES";
-				// const end = "BASIC";
+				// const end = "";
 				// const responsibilties = job_info.split(start)[1]?.split(end)[0];
 				// console.log("responsibilities", responsibilties);
 				final_costar.push(new_obj);
@@ -119,7 +120,149 @@ let costar = async () => {
 	return final_costar;
 };
 
+let parsons = async () => {
+	let count = 0;//tracks current page of info pulled
+	let status;//true if current fetch request was 200 level response, false otherwise
+	let final_parsons = [];//final JSON var containing array of dicts, where each element contains dict with keys (name,link,details)
+	let totatHits = 0;//total # of job listings
+	let promise_arr;
+
+	console.log();
+	let origin = await fetch("https://jobsapi-google.m-cloud.io/api/job/search?callback=jobsCallback&pageSize=1&offset=24&companyName=companies%2Fedb4e875-81c8-4aa4-bb57-34d7c0f7fc5c&orderBy=posting_publish_time%20desc")
+		.then(res => {
+			status = res.ok;
+			console.log("res value", status);
+			return res;
+		});
+	// if (!status)
+	// 	break;
+	let src = await origin.text();
+	const start = "jobsCallback(";
+	const end = ")";
+	const middleText = src.split(start)[1]?.split(end)[0];
+	console.log("middle text", src.substring(13, src.length - 1))
+	let obj = JSON.parse(src.substring(13,src.length-1));
+	console.log("object for this endpoint", obj);
+	totatHits = obj.totatHits;totatHits=parseInt(totatHits);
+	console.log("count var", count++);
+	// const parser = new DOMParser();
+	// const htmlDoc = parser.parseFromString(src, 'text/html');
+
+	origin = await fetch("https://jobsapi-google.m-cloud.io/api/job/search?callback=jobsCallback&pageSize="+24+"&offset=24&companyName=companies%2Fedb4e875-81c8-4aa4-bb57-34d7c0f7fc5c&orderBy=posting_publish_time%20desc")
+		.then(res => {
+			status = res.ok;
+			console.log("res value", status);
+			return res;
+		});
+	src = await origin.text();
+	// console.log("middle text", src.substring(13, src.length - 1))
+	obj = JSON.parse(src.substring(13, src.length - 1));
+	console.log("object for this endpoint", obj);
+	totatHits = obj.totatHits;
+	let job_arr = obj.searchResults;
+
+	for(let i = 0; i < job_arr.length; i++) {
+		let new_obj = {};
+		let curr_elem = job_arr[i];
+		let name = curr_elem.job.name;//gets job titls
+		let link = curr_elem.job.url;//get link to apply 
+		let job_info = curr_elem.job.description;
+		job_info = job_info.replace(/<\/?[^>]+(>|$)/g, "");
+		new_obj.name = name;
+		new_obj.link = link;
+		new_obj.job_info = job_info;
+		final_parsons.push(new_obj);
+	}
+
+	console.log("final parsons", final_parsons)
+
+
+
+
+	// //got next 50 elements, each element of array is job
+	// promise_arr = [];//contains array of promises
+	// let job_arr = obj.body.children[0].children[0].listItems;
+	// if (!job_arr)
+	// 	continue;
+	// console.log("job_arr", job_arr)
+	// for (let i = 0; i < job_arr.length - 1; i++) {
+	// 	let curr_promise = new Promise(async (resolve1, reject1) => {
+	// 		let new_obj = {};
+	// 		let curr_elem = job_arr[i];
+	// 		let name = curr_elem.title.instances[0].text;//gets name
+	// 		let link = curr_elem.title.commandLink;//get link to apply 
+	// 		console.log("link", link);
+	// 		//use to fetch the next page, second level down
+	// 		const origin2 = await fetch("https://costar.wd1.myworkdayjobs.com" + link, {
+	// 			"headers": {
+	// 				"accept": "application/json,application/xml",
+	// 				"accept-language": "en-US,en;q=0.9",
+	// 				"content-type": "application/x-www-form-urlencoded",
+	// 				"sec-ch-ua": "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+	// 				"sec-ch-ua-mobile": "?0",
+	// 				"sec-ch-ua-platform": "\"Windows\"",
+	// 				"sec-fetch-dest": "empty",
+	// 				"sec-fetch-mode": "cors",
+	// 				"sec-fetch-site": "same-origin",
+	// 				"x-workday-client": "2021.39.009"
+	// 			},
+	// 			"referrer": "https://costar.wd1.myworkdayjobs.com/en-US/CoStarCareers/job/CA-ON-Toronto/Sales-Associate--CoStar---CA-ON-Toronto_R23961",
+	// 			"referrerPolicy": "no-referrer-when-downgrade",
+	// 			"body": null,
+	// 			"method": "GET",
+	// 			"mode": "cors",
+	// 			"credentials": "include"
+	// 		});
+	// 		let src2 = await origin2.text();
+	// 		const obj_job = JSON.parse(src2); console.log("obj_job", obj_job, obj_job.body.children[1].children[0].children[2])
+	// 		let job_info = obj_job.body.children[1].children[0].children[2].text;//gets complete HTML string containing Job description, Responsibilties, Basic Qualifactions,
+
+	// 		new_obj.name = name;
+	// 		new_obj.link = link;
+	// 		new_obj.job_info = job_info;
+	// 		// const start = "RESPONSIBILITIES";
+	// 		// const end = "BASIC";
+	// 		// const responsibilties = job_info.split(start)[1]?.split(end)[0];
+	// 		// console.log("responsibilities", responsibilties);
+	// 		final_costar.push(new_obj);
+	// 		var rand_amt = getRandomIntInclusive(1, 4);
+
+	// 		// sleep(rand_amt * 1000);
+
+	// 		resolve1();
+	// 	});
+	// 	promise_arr.push(curr_promise);
+	// }
+
+
+	// // } while (status)
+
+	// const data = await Promise.allSettled(promise_arr)
+
+	// console.log("costar JSON data", final_costar)
+
+	myHeaders = new Headers();
+	// myHeaders.set("Accept", "text/html, */*; q=0.01");
+
+	let formdata = new FormData();
+	formdata.append("JSON_data", JSON.stringify(final_parsons));
+	formdata.append("platform", "Parsons");
+
+
+	requestOptions = {
+		method: 'POST',
+		headers: myHeaders,// need to populate headers for web service will necessary info
+		body: formdata,
+		redirect: 'follow'
+	};
+
+	let send_info = await fetch("http://127.0.0.1:5000/receive", requestOptions);
+
+	return final_parsons;
+}
+
 costar();
 
+parsons();
 
 
--- Flask App/app.py
+++ Flask App/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, flash, request, redirect, jsonify, url_for, render_template
 import pymysql
 import requests
@@ -23,10 +22,62 @@ def index():
 
 @app.route('/submit')
 def submit():
-    form = request.form
-    print("INSIDE SUBMIT",form)
+    print("INSIDE SUBMIT",request.args, request.args.get('fname'))
+    skills = request.args.get('skills')
+    degree = request.args.get('degree')
+    major = request.args.get('major')
+    location = request.args.get('location')
+
+    main_JSON = []#main JSON to display to user
+
+    #search through Costar DATA
+    with open('jobs_Costar.json') as f:
+        costar_data = json.load(f)
+
+    with open('jobs_Parsons.json') as f:
+        parsons_data = json.load(f)
+
+    for x in costar_data:
+        if(location in x['name'] or location in x['job_info'] or location in x['link']):
+            x['Platform'] = 'Costar'
+            main_JSON.append(x)
+            continue
+        if(major in x['name'] or major in x['job_info'] or major in x['link']):
+            x['Platform'] = 'Costar'
+            main_JSON.append(x)
+            continue
+        if(degree in x['name'] or degree in x['job_info'] or degree in x['link']):
+            x['Platform'] = 'Costar'
+            main_JSON.append(x)
+            continue
+        if(skills in x['name'] or skills in x['job_info'] or skills in x['link']):
+            x['Platform'] = 'Costar'
+            main_JSON.append(x)
+            continue
+
+    #search through Parsons data
+    for x in parsons_data:
+        if(location in x['name'] or location in x['job_info'] or location in x['link']):
+            x['Platform'] = 'Parsons'
+            main_JSON.append(x)
+            continue
+        if(major in x['name'] or major in x['job_info'] or major in x['link']):
+            x['Platform'] = 'Parsons'
+            main_JSON.append(x)
+            continue
+        if(degree in x['name'] or degree in x['job_info'] or degree in x['link']):
+            x['Platform'] = 'Parsons'
+            main_JSON.append(x)
+            continue
+        if(skills in x['name'] or skills in x['job_info'] or skills in x['link']):
+            x['Platform'] = 'Parsons'
+            main_JSON.append(x)
+            continue
+
+    print(main_JSON[0])
+
     # , columns=['a', 'b', 'c'], table_data={"1": {"col1": "1", "col2": "2", "col3": "3"}, "2": {"col1": "113", "col2": "212", "col3": "354"}})
-    return render_template('data.html', bulletins = [{'title': 'HIII', 'date': 'DATEE VALUE'}, {'title': 'HIII AGAIN', 'date': 'THE SECOND VALUE'}])
+    return render_template('index2.html', bulletins=main_JSON)
 
 
 @app.route('/receive', methods=['GET', 'POST'])
@@ -43,8 +94,19 @@ def receive():
     else:
         print("The file does not exist")
 
-    open(file_path,'x')
-    with open(file_path, 'w') as outfile:
+    # try:
+    #     os.mkdir(file_path)
+    # except FileExistsError as e:
+    #     print("TRIED MAKING DIR")
+    #     print(e)
+    #     pass
+    if os.path.exists('jobs_'+platform+'.json'):
+        os.remove('jobs_'+platform+'.json')
+    else:
+        print("Can not delete the file as it doesn't exists")
+    
+    # open(file_path,'x')
+    with open('jobs_'+platform+'.json', 'x') as outfile:
         json.dump(JSON_data, outfile)
     
     return json.dumps(JSON_data)
