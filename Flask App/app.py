0268399ad1bd72ae633d6101fd5e5fd4b4171d61
omaramr3410 <omaramr3410@gmail.com>
2021-09-26 08:56:39 -0400

    updating costar again, again

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask import Flask, flash, request, redirect, jsonify, url_for
+
+from flask import Flask, flash, request, redirect, jsonify, url_for, render_template
 import pymysql
 import requests
 import pandas as pd
@@ -9,6 +9,8 @@ import json
 from datetime import datetime, timedelta, date
 import numpy as np
 import getpass
+import os
+import json
 
 app = Flask(__name__)
 
@@ -16,40 +18,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     print("asdknasljkbnsdajklhasdjkhhbdsajk")
-    return '''<form action="/submit">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" value="Mike"><br><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" value="Walker"><br><br>
-        <input type="submit" value="Submit">
-        </form>'''
-    # return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/submit')
 def submit():
-    print("INSIDE SUBMIT")
-    return '''<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!--Meta Info-->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>My Website</title>
-    
-    <!-- Website Icon -->
-    <link rel="icon" href="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" type="x_icon">    
-    <!-- import the webpage's stylesheet -->
-    <link rel="stylesheet" href="style.css">
-
-    <!-- import the webpage's javascript file -->
-    <script src="script.js" defer></script>
-  </head>  
-  <body>
-  </body>
-</html>'''
+    form = request.form
+    print("INSIDE SUBMIT",form)
+    # , columns=['a', 'b', 'c'], table_data={"1": {"col1": "1", "col2": "2", "col3": "3"}, "2": {"col1": "113", "col2": "212", "col3": "354"}})
+    return render_template('data.html', bulletins = [{'title': 'HIII', 'date': 'DATEE VALUE'}, {'title': 'HIII AGAIN', 'date': 'THE SECOND VALUE'}])
 
 
 @app.route('/receive', methods=['GET', 'POST'])
@@ -58,6 +35,16 @@ def receive():
     JSON_data = request.form.get('JSON_data')
     JSON_data = json.loads(JSON_data)
     platform = request.form.get('platform')
-    print("OUR COSTAR DATA", JSON_data, platform)
+    print("OUR COSTAR DATA", type(JSON_data), platform)
     #save JSON data as JSON file locally
-    return JSON_data
+    file_path = 'platform_jobs/'+'jobs_'+platform+'.json'
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        print("The file does not exist")
+
+    open(file_path,'x')
+    with open(file_path, 'w') as outfile:
+        json.dump(JSON_data, outfile)
+    
+    return json.dumps(JSON_data)
