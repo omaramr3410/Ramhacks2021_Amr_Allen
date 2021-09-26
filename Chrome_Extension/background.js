3dad75f690e48991b4f46ee3a616314a2591c2ee
omaramr3410 <omaramr3410@gmail.com>
2021-09-25 23:15:50 -0400

    updating costar

--- Chrome_Extension/background.js
+++ Chrome_Extension/background.js
@@ -1,10 +1,25 @@
 console.log("running background");
+// simple function to hang up code for x ms, temporary
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 
 let costar = async () => {
 	let count = 0;//tracks current page of info pulled
 	let status;//true if current fetch request was 200 level response, false otherwise
 	let final_costar = [];//final JSON var containing array of dicts, where each element contains dict with keys (name,link,details)
 	do{
+		console.log();
 		let origin = await fetch("https://costar.wd1.myworkdayjobs.com/CoStarCareers/8/searchPagination/318c8bb6f553100021d223d9780d30be/"+(count+1)*50)
 			.then(res=>{
 				status = res.ok;
@@ -21,28 +36,55 @@ let costar = async () => {
 		
 		//got next 50 elements, each element of array is job
 		let promise_arr = [];//contains array of promises
-		let job_arr = obj.body.children[0]..listitems;
+		let job_arr = obj.body.children[0].children[0].listItems;
+		if(!job_arr)
+			continue;
+		console.log("job_arr", job_arr)
 		for(let i=0;i<job_arr.length-1;i++){
 			let curr_promise = new Promise(async (resolve1, reject1) =>{
 				let new_obj = {};
-				let curr_elem = jobs_arr[i];
+				let curr_elem = job_arr[i];
 				let name = curr_elem.title.instances.text;//gets name
 				let link = curr_elem.title.commandLink;//get link to apply 
-
+				console.log("link", link);
 				//use to fetch the next page, second level down
-				const origin2 = await fetch("https://costar.wd1.myworkdayjobs.com/"+link);
+				const origin2 = await fetch("https://costar.wd1.myworkdayjobs.com"+link, {
+					"headers": {
+						"accept": "application/json,application/xml",
+						"accept-language": "en-US,en;q=0.9",
+						"content-type": "application/x-www-form-urlencoded",
+						"sec-ch-ua": "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+						"sec-ch-ua-mobile": "?0",
+						"sec-ch-ua-platform": "\"Windows\"",
+						"sec-fetch-dest": "empty",
+						"sec-fetch-mode": "cors",
+						"sec-fetch-site": "same-origin",
+						"x-workday-client": "2021.39.009"
+					},
+					"referrer": "https://costar.wd1.myworkdayjobs.com/en-US/CoStarCareers/job/CA-ON-Toronto/Sales-Associate--CoStar---CA-ON-Toronto_R23961",
+					"referrerPolicy": "no-referrer-when-downgrade",
+					"body": null,
+					"method": "GET",
+					"mode": "cors",
+					"credentials": "include"
+				});
 				let src2 = await origin2.text();
-				const obj_job = JSON.parse(src2);
-				let job_info = obj_job.body.children[1].children[2].text;//gets complete HTML string containing Job description, Responsibilties, Basic Qualifactions, 
+				const obj_job = JSON.parse(src2); console.log("obj_job", obj_job, obj_job.body.children[1].children[0].children[2])
+				let job_info = obj_job.body.children[1].children[0].children[2].text;//gets complete HTML string containing Job description, Responsibilties, Basic Qualifactions,
 
 				new_obj.name = name;
 				new_obj.link = link;
 				new_obj.job_info = job_info;
-				const start = "RESPONSIBILITIES";
-				const end = "BASIC";
-				const responsibilties = job_info.split(start)[1]?.split(end)[0];
-				console.log("")
+				// const start = "RESPONSIBILITIES";
+				// const end = "BASIC";
+				// const responsibilties = job_info.split(start)[1]?.split(end)[0];
+				// console.log("responsibilities", responsibilties);
 				final_costar.push(new_obj);
+				var rand_amt = getRandomIntInclusive(1,4); 
+
+				// sleep(rand_amt * 1000);
+
+				resolve1();
 			});
 			promise_arr.push(curr_promise);
 		}
@@ -50,8 +92,11 @@ let costar = async () => {
 
 	}while(status)
 
-	return;
+	console.log("costar JSON data",final_costar)
+	return final_costar;
 };
 
 costar();
+
+
 
